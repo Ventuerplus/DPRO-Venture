38bf18b6ff62fd08cd29044ca452ee8b68e0cfaf
Ventuerplus <it@ventureuae.com>
2024-09-16 12:46:29 +0400

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Unity;
+using ventureerp.Models;
+using ventureerp.Services;
+
+namespace ventureerp
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            //RouteTable.Routes.MapHubs();
+
+            GlobalConfiguration.Configure(WebApiConfig.Register); // <--- this MUST be first 
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //BundleTable.EnableOptimizations = true;
+            Database.SetInitializer<ventureerpContext>(null);
+
+            var container = new UnityContainer();
+            container.RegisterType<IPayrollService, PayrollService>();
+
+        }
+        
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(ventureerp.Startup))]
+namespace ventureerp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
